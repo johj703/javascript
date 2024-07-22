78cccc58f30b94be7d1da07643a79ab33c9833bd
Jo <johj703@nate.com>
2024-07-22 21:32:11 +0900

    문자열을 자르는 substr 사용 방법은 첫 번째, 첫 번째 인자는 제일 앞 칸에서 몇 칸 뒤로 이동 한다. 두 번째, 첫 번째 인자에서 이동한 칸에서 두 번째 인자의 숫자만큼을 이동한 만큼을 자른다.로 이해하면 된다!

--- 03.js
+++ 03.js
@@ -12,3 +12,7 @@ let str1 = "Hello, ";
 let str2 = "world!";
 let result = str1.concat(str2);
 // console.log(result);
+
+// 2-3. 문자열 자르기
+let str3 = "Hello, World!";
+console.log(str3.substr(7, 5));
