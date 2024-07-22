b66d3ac3999d4a9cf1ab6dc09a959ad43bf1514b
Jo <johj703@nate.com>
2024-07-22 21:57:48 +0900

    undefined는 변수 선언은 했지만 할당을 하지 않으면 나타나는 타입이다!

--- 04.js
+++ 04.js
@@ -3,7 +3,12 @@
 let bool1 = true;
 let bool2 = false;
 
-console.log(bool1);
-console.log(typeof bool1);
-console.log(bool2);
-console.log(typeof bool2);
+// console.log(bool1);
+// console.log(typeof bool1);
+// console.log(bool2);
+// console.log(typeof bool2);
+
+// undefined
+// un : not, define: 정의하다
+let x;
+console.log(x);
