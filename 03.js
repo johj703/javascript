f72a42928fc4b78a2de2dec43c5ac0ac12bba11c
Jo <johj703@nate.com>
2024-07-22 21:23:36 +0900

    .length는 문자열의 길이 확인하기 함수!

--- 03.js
+++ 03.js
@@ -1,5 +1,8 @@
 // 2. 문자 : string(문자열 = 문자의 나열)
 // ' ' = " "
 let str = "Hello World!";
-console.log(str);
-console.log(typeof str);
+// console.log(str);
+// console.log(typeof str);
+
+// 2-1. 문자열 길이 확인하기
+console.log(str.length);
