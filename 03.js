b83ca7ca2d0c78ad40bf0b7c7c1c3527f83744a8
Jo <johj703@nate.com>
2024-07-22 21:27:30 +0900

    주석 처리

--- 03.js
+++ 03.js
@@ -5,10 +5,10 @@ let str = "Hello World!";
 // console.log(typeof str);
 
 // 2-1. 문자열 길이 확인하기
-console.log(str.length);
+// console.log(str.length);
 
 // 2-2. 문자열 결합하기(Concatenation)
 let str1 = "Hello, ";
 let str2 = "world!";
 let result = str1.concat(str2);
-console.log(result);
+// console.log(result);
