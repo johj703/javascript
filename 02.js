703392d661cd3a2f8d4b7bfff125a2c9036bfff0
Jo <johj703@nate.com>
2024-07-22 21:08:07 +0900

    자바스크립트에서 데이터 타입을 확인하는 방법은 typeof를 사용하는 것이다!

---
+++ 02.js
@@ -0,0 +1,13 @@
+// 데이터 타입
+// runtime : run 하는 time
+// 코드를 작성 할 때가 아니라, 실제 코드가 실행 될 때
+// -> 옆에 있는 터미널에 코드가 실행 될 때,
+// 그 때, 데이터 타입이 결정된다.
+// Java : String a = "abc";
+// const a = "abc";
+
+// 1. 숫자
+// 1-1. 정수
+let num1 = 10;
+console.log(num1);
+console.log(typeof num1);
