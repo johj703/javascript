b54506897e91d500f75b09a16704322541fd7177
Jo <johj703@nate.com>
2024-07-23 15:06:38 +0900

    숫자와 문자열이 합쳐졌는데 합친 값의 타입이 string으로 변한다! 2

--- 05.js
+++ 05.js
@@ -5,5 +5,9 @@
 // 1. 암시적
 // 1-1. 문자열
 let result1 = 1 + "2";
-console.log(result1);
-console.log(typeof result1);
+// console.log(result1);
+// console.log(typeof result1);
+
+let result2 = "1" + true;
+console.log(result2);
+console.log(typeof result2);
