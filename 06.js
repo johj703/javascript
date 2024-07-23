fd696f42da6e9593762deafa75a3daaf48bc15a0
Jo <johj703@nate.com>
2024-07-23 16:45:54 +0900

    할당 연산자

--- 06.js
+++ 06.js
@@ -1,21 +1,40 @@
 // 연산자(+, -, *, / %)
 
 // 1. 더하기 연산자(+)
-console.log(1 + 1);
-console.log(1 + "1");
+// console.log(1 + 1);
+// console.log(1 + "1");
 
 // 2. 빼기 연산자(-)
-console.log(1 - "2"); // -1
-console.log(1 - 2); // -1
+// console.log(1 - "2"); // -1
+// console.log(1 - 2); // -1
 
 // 3. 곱하기 연산자(*)
-console.log(2 * 3); // 6
-console.log("2" * 3); // 6
+// console.log(2 * 3); // 6
+// console.log("2" * 3); // 6
 
 // 4. 나누기 연선자(/)
-console.log(4 / 2); // 2
-console.log("4" / 2); // 2
+// console.log(4 / 2); // 2
+// console.log("4" / 2); // 2
 
 // 5. 나누기 연산자(/) vs 나머지 연산자(%)
-console.log(5 / 2); // 2.5
-console.log(5 % 2); // 1
+// console.log(5 / 2); // 2.5
+// console.log(5 % 2); // 1
+
+// 6. 할당 연산자(assignment)
+// 6-1. 등호 연산자(=)
+let x = 10;
+console.log(x);
+
+// 6-2. 더하기 등호 연산자(+=);
+x += 5;
+console.log(x);
+
+// 6-3. 빼기 등호 연산자(-=)
+x -= 5;
+// x = x -5;
+console.log(x);
+
+// 여기서 x를 -10으로
+// x -= 20;
+x = x - 20;
+console.log(x);
