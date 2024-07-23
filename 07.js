471344054575f5a916e3bf4701326f4e9d535f8b
Jo <johj703@nate.com>
2024-07-23 17:37:45 +0900

    함수 표현식

--- 07.js
+++ 07.js
@@ -6,3 +6,15 @@
 function add(x, y) {
   return x + y;
 }
+
+// 2. 함수 표현식
+let add2 = function (x, y) {
+  return x + y;
+};
+
+// 함수를 호출한다(= 사용한다)
+// 함수명() -> add(입력값);
+// console.log(add(2, 3));
+
+let functionResult = add(3, 4);
+console.log(functionResult);
