1614f244e0ba925920082b01a8196c1cf9c2162a
Jo <johj703@nate.com>
2024-07-23 15:41:44 +0900

    더하기 연산자를 제외한 빼기 곱하기 나누기 연산자에서 숫자 타입이 우선시 된다

--- 05.js
+++ 05.js
@@ -9,5 +9,16 @@ let result1 = 1 + "2";
 // console.log(typeof result1);
 
 let result2 = "1" + true;
-console.log(result2);
-console.log(typeof result2);
+// console.log(result2);
+// console.log(typeof result2);
+
+// {}, null, undefined + "1" => 문자열
+
+// 1-2/ 숫자
+let result3 = 1 - "2";
+// console.log(result3);
+// console.log(typeof result3);
+
+let result4 = "2" * "3";
+console.log(result4);
+console.log(typeof result4);
