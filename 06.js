f81db1f33c22ca2c5b29f00c39ce26eed8f1197c
Jo <johj703@nate.com>
2024-07-23 17:16:29 +0900

    비교 연산자

--- 06.js
+++ 06.js
@@ -44,3 +44,46 @@
 // a *= 2;
 
 // console.log(a);
+
+// 비교 연산자(---> true 또는 false를 반환하는 연산자)
+// 1. 일치 연산자(===)
+// 타입까지 일치해야 true를 반환하는 연산자
+
+// 숫자 2가 숫자 2랑 같니? 응!
+console.log(2 === 2); // true
+console.log("2" === 2); // false
+console.log(2 === "2"); // false
+
+// 2. 불일치 연산자(!==)
+// 타입까지 일치해야 false를 반환하는 연산자
+console.log(2 !== 2); // false
+console.log("20" !== 2); // true
+console.log(2 !== "2"); // true
+
+// 3. 작다 연산자(<)
+console.log("--------");
+console.log(2 < 3); // true
+console.log(2 <= 3); // true
+console.log(3 <= 3); // true
+console.log(4 <= 3); // false
+
+// 4. 논리 연산자
+// 4-1. 논리곱 연산자 : 모두 true일 때, true 반환
+console.log("---------");
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
+
+// 4-2. 논리합 연산자 : 두 값 중 하나라도 true인 경우 true 반환
+console.log("---------");
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+// 4-3. 논리부정 연산자: 값을 반대로 바꿈
+console.log("---------");
+console.log(!true);
+let a = true;
+console.log(!a);
