e3a8a3d137ff238fc57692cfb18452b2fe693cf6
Jo <johj703@nate.com>
2024-07-23 16:01:24 +0900

    Boolean 타입의 명시적 형변환 확인

--- 05.js
+++ 05.js
@@ -20,5 +20,15 @@ let result3 = 1 - "2";
 // console.log(typeof result3);
 
 let result4 = "2" * "3";
-console.log(result4);
-console.log(typeof result4);
+//
+
+// 2. 명시적 형 변환
+// 2-1. Boolean
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));
+console.log("---------------------");
+console.log(Boolean("false"));
+console.log(Boolean({}));
