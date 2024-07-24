eafd050a3af33692f270a70e87ed3679837642f1
Jo <johj703@nate.com>
2024-07-24 09:52:11 +0900

    if - else문 형태

--- 09.js
+++ 09.js
@@ -1,9 +1,27 @@
 // 조건문 - if, else if, else, switch
 // 1. if문
-let x = 10;
+// let x = 10;
 
 // 1-1
+// if (x > 0) {
+//   // main logic
+//   console.log("x는 함수입니다.");
+// }
+
+// 1-2
+// let y = "hello world";
+
+// // y의 길이가 5보다 크거나 같으면 길이를 console.log로 출력
+// if (y.length >= 5) {
+//   console.log(y.length);
+// }
+
+let x = 10;
+// 2. if - else문
 if (x > 0) {
-  // main logic
-  console.log("x는 함수입니다.");
+  // main login #1
+  console.log("x는 양수 입니다.");
+} else {
+  // main logic #2
+  console.log("x는 음수 입니다.");
 }
