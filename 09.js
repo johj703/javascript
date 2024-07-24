62207a62e32a7110fb986184b2f315decc9aa67a
Jo <johj703@nate.com>
2024-07-24 14:26:30 +0900

    switch문에서 하나의 case가 끝날 때에는 break를 넣어줘야 한다!

--- 09.js
+++ 09.js
@@ -43,15 +43,19 @@
 // 1-4. switch
 // 변수의 값에 따라, 여러 개의 경우(case) 중 하나를 선택
 // default
-let fruit = "사과";
+let fruit = "메론";
 
 switch (fruit) {
   case "사과":
     console.log("사과 입니다.");
+    break;
   case "바나나":
     console.log("바나나 입니다.");
+    break;
   case "키위":
     console.log("키위 입니다.");
+    break;
   default:
     console.log("아무것도 아닙니다.");
+    break;
 }
