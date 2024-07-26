b6d76c85e64b6072297c3c2b340f46d936c79bcc
Jo <johj703@nate.com>
2024-07-26 21:31:13 +0900

    배열에서 새로운 요소를 추가할 때는 push를 사용한다.

--- 16.js
+++ 16.js
@@ -1,7 +1,7 @@
 // 배열
 // 1. 생성
 // 1-1. 기본 생성
-let fruits = ["사과", "바나나", "오렌지"];
+// let fruits = ["사과", "바나나", "오렌지"];
 
 // 1-2. 크기 지정
 let number = new Array(5);
@@ -10,6 +10,14 @@ let number = new Array(5);
 // console.log(number.length);
 
 // 2. 요소 접근
-console.log(fruits[0]);
-console.log(fruits[1]);
-console.log(fruits[2]);
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+
+// 3. 배열 메소드
+// 3-1. push
+let fruits = ["사과", "바나나"];
+console.log("1 => ", fruits);
+
+fruits.push("오렌지");
+console.log("2 => ", fruits);
