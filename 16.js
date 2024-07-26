c91d79d54f5ebd611d04dac4a73f8f1d4a1aad93
Jo <johj703@nate.com>
2024-07-26 20:59:22 +0900

    배열의 요소로 접근 할때에는 배열의 index 번호를 넣어주면 된다!

--- 16.js
+++ 16.js
@@ -6,5 +6,8 @@ let fruits = ["사과", "바나나", "오렌지"];
 // 1-2. 크기 지정
 let number = new Array(5);
 
-console.log(fruits.length);
-console.log(number.length);
+// console.log(fruits.length);
+// console.log(number.length);
+
+// 2. 요소 접근
+console.log(fruits[0]);
