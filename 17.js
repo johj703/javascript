76c7282183f13f14dcc6f2efb848980013623ef0
Jo <johj703@nate.com>
2024-07-27 23:28:58 +0900

    for ~ in문 예시

--- 17.js
+++ 17.js
@@ -18,11 +18,23 @@
 // }
 
 //ex: 0부터 10까지의 수 중에서 2의 배수만 console.log로 출력하는 예시
-for (let i = 0; i < 11; i++) {
-  if (i >= 2) {
-    if (i % 2 === 0) {
-      // 2의 배수
-      console.log(i + "는 2의 배수 입니다!!");
-    }
-  }
+// for (let i = 0; i < 11; i++) {
+//   if (i >= 2) {
+//     if (i % 2 === 0) {
+//       // 2의 배수
+//       console.log(i + "는 2의 배수 입니다!!");
+//     }
+//   }
+// }
+
+// for ~ in문
+// 객체 속성을 출력하는 문법
+let person = {
+  name: "John",
+  age: 30,
+  gender: "male",
+};
+
+for (let key in person) {
+  console.log(key + " : " + person[key]);
 }
