d4e0b64a76e9f6658e2f4ee5bdf86cc6a5348328
Jo <johj703@nate.com>
2024-07-27 23:51:26 +0900

    continue는 다음 순서로 넘어가게 하는 명령문이다.

--- 18.js
+++ 18.js
@@ -29,9 +29,16 @@
 //   i++;
 // } while (i < 10);
 
+// for (let i = 0; i <= 10; i++) {
+//   if (i === 5) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
 for (let i = 0; i <= 10; i++) {
   if (i === 5) {
-    break;
+    continue;
   }
   console.log(i);
 }
