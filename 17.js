23e61bf5ae102e17942d262c1a52663b569b6f91
Jo <johj703@nate.com>
2024-07-27 22:44:52 +0900

    배열과 for문은 쿵짝이 잘 맞는다!

--- 17.js
+++ 17.js
@@ -6,6 +6,13 @@
 // i라는 변수는 0부터 시작할꺼야!
 // i라는 변수가 10에 도달하기 전까지 계속 할꺼야!
 // i라는 변수는 한 사이클이 돌고 나면 1을 더할꺼야!
-for (let i = 0; i < 10; i++) {
-  console.log("for문이 돌아가고 있음 => " + i);
+// for (let i = 0; i < 10; i++) {
+//   console.log("for문이 돌아가고 있음 => " + i);
+// }
+
+// 배열과 for문은 짝꿍이다.
+const arr = ["one", "two", "three", "four", "five"];
+for (let i = 0; i < arr.length; i++) {
+  console.log(i);
+  console.log(arr[i]);
 }
