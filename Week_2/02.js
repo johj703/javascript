1aaba21db2ed5140826bc9f9f085043b25a4ec85
Jo <johj703@nate.com>
2024-07-29 20:13:26 +0900

    구조분해할당 예시2

--- Week_2/02.js
+++ Week_2/02.js
@@ -4,6 +4,13 @@
 // 배열이나 객체의 속성
 
 // (1) 배열의 경우
-let [value1, value2] = [1, "new"];
-console.log("1", value1);
-console.log("2", value2);
+// let [value1, value2] = [1, "new"];
+// console.log("1", value1);
+// console.log("2", value2);
+
+let arr = ["value1", "value2", "value3"];
+let [a, b, c] = arr;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
