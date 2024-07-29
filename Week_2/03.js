1610b28fd9680d668b3470ccd7154a44b138a7ff
Jo <johj703@nate.com>
2024-07-29 21:16:14 +0900

    주석 처리

--- Week_2/03.js
+++ Week_2/03.js
@@ -8,10 +8,10 @@ const obj1 = { name: name, age: age };
 
 // 전개 구문 = spread operator
 // destructuring과 함께 가장 많이 사용되는 es6 문법 중 하나!
-let arr = [1, 2, 3];
-console.log(arr);
-console.log(...arr);
+// let arr = [1, 2, 3];
+// console.log(arr);
+// console.log(...arr);
 
-let newArr = [...arr, 4];
-console.log(arr);
-console.log(newArr);
+// let newArr = [...arr, 4];
+// console.log(arr);
+// console.log(newArr);
