7877820cb2f92d24c46547c34958b6319f968bf4
Jo <johj703@nate.com>
2024-07-29 21:13:15 +0900

    spread operator를 사용하면 대괄호가 없어지고 배열의 요소만 나타나게 된다.

--- Week_2/03.js
+++ Week_2/03.js
@@ -5,3 +5,9 @@ const age = "30";
 // key - value가 같으면 생략할 수 있다!
 const obj = { name, age };
 const obj1 = { name: name, age: age };
+
+// 전개 구문 = spread operator
+// destructuring과 함께 가장 많이 사용되는 es6 문법 중 하나!
+let arr = [1, 2, 3];
+console.log(arr);
+console.log(...arr);
