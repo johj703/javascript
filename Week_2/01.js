07d8447403cf947b9f5e0926dc1029b2e27494f4
Jo <johj703@nate.com>
2024-07-29 20:00:53 +0900

    함수 선언문, 화살표 함수

--- Week_2/01.js
+++ Week_2/01.js
@@ -2,3 +2,18 @@
 
 // 2015년도 이전에는 => var 사용
 // (1) es6 => let(변수), const(상수)
+
+// (2) arrow function
+// 함수 선언문
+function add() {}
+
+var add = function () {};
+
+// arrow function
+var add = () => {
+  return 1;
+};
+
+var add = () => 1;
+
+var add = (x) => 1;
