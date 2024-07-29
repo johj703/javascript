761fbc4d3ddb5b23c3cbf3b94bf148fd27b15be7
Jo <johj703@nate.com>
2024-07-29 20:11:19 +0900

    구조분해할당 예시

---
+++ Week_2/02.js
@@ -0,0 +1,9 @@
+// 구조분해할당 : destructuring(de + structure + ing)
+// de = not
+// structure = 구조
+// 배열이나 객체의 속성
+
+// (1) 배열의 경우
+let [value1, value2] = [1, "new"];
+console.log("1", value1);
+console.log("2", value2);
